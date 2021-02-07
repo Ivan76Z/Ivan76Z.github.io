375cd6d1b16226387dd92d53c383274248984ef6
Ivan Zoyko <Ivan76Z@yandex.ru>
2021-02-07 22:20:19 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+$( document ).ready(function() {
+	var template,data;
+	$.getJSON('data.json', function(data) {
+		//let data = JSON.stringify(obj);
+    for(let i =1;i<=data.length+1;i++){
+    	$('#tbody').append(`
+    		
+                <tr role="row" class="odd" data-id="${data[i-1]["id"]}">
+                    <td>${i}</td>
+                    <td style=" left: 58px;">
+                        <div>${data[i-1]["title"]}</div>
+                    </td>
+                    <td>${data[i-1]["amount"]}</td>
+                    <td>${data[i-1]["price"]}</td>
+                    <td>${data[i-1]["id"]}</td>
+                </tr>
+
+    		`);
+    }
+});
+
+
+});
