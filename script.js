e356b7c200d369fa88ad6d5e3a361b8769c58816
Ivan Zoyko <Ivan76Z@yandex.ru>
2021-03-15 23:14:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $( document ).ready(function() {
-	document.querySelectorAll(".max_price")[0].html="34343434";
+	document..getElementById("#max_price").html="34343434";
 	var template,data;
 	var max = 0;
 	var total_price = 0;
@@ -24,9 +24,9 @@ $( document ).ready(function() {
 	    		console.log("max= " + max);
 		console.log("total_price= " + total_price);
 		console.log("total_items= " + i);
-		document.querySelectorAll(".max_price")[0].html=max;
-		document.querySelectorAll(".total_price")[0].html=total_price;
-		document.querySelectorAll(".total_items")[0].html=i;
+		document.getElementById("#max_price").html=max;
+		document.getElementById("#total_price").html=total_price;
+		document.getElementById("#total_items").html=i;
     }
 
 		
