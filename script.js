cd8139e591221fbd49471bc3eb75d32711e6737a
Ivan Zoyko <Ivan76Z@yandex.ru>
2021-03-15 22:53:41 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 $( document ).ready(function() {
 	var template,data;
+	var max = 0;
+	var total_price = 0;
 	$.getJSON('data.json', function(data) {
 		//let data = JSON.stringify(obj);
-    for(let i =1;i<=data.length+1;i++){
+    for(var i =1;i<=data.length+1;i++){
+	    if(data[i-1]["price"]>max){max=data[i-1]["price"]}
+	    total_price += data[i-1]["price"];
     	$('#tbody').append(`
     		
                 <tr role="row" class="odd" data-id="${data[i-1]["id"]}">
@@ -16,6 +20,10 @@ $( document ).ready(function() {
 
     		`);
     }
+		$('.max_price').text(max);
+		$('.total_items').text(i);
+		$('.total_price').text(total_price);
+		
 });
 
 
