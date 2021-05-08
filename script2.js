f4ca275700e9ad4cbd67b85ef7f351dfa1d9eaaa
Ivan Zoyko <Ivan76Z@yandex.ru>
2021-05-08 23:48:11 +0300

    Update script2.js

--- script2.js
+++ script2.js
@@ -69,7 +69,7 @@ $(document).ready(function() {
         var chartOptions = {
             legend: {
                 display: true,
-                boxWidth: 10;
+                boxWidth: 10,
                 position: 'top',
                 labels: {
                     boxWidth: 80,
