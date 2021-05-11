68725bf66bc77af611a983a7238e9d40a35c14bc
Ivan Zoyko <Ivan76Z@yandex.ru>
2021-05-12 00:57:46 +0300

    Update login.js

--- login.js
+++ login.js
@@ -5,9 +5,10 @@ $(document).ready(function() {
 <h1 style="margin: 40px auto;padding: 20px 0 20px 0px;">Авторизация</h2>
 <center>
 <h2 style="margin: 50px 0 0 0;">Вход</h2>
-<input placeholder="Логин" type="text" id="form_login">
-<input placeholder="Пароль" type="password" id="form_password">
-<div class="form-button login_btn">Войти</div>
+<input placeholder="Логин" type="text" id="form_login" style="display: block;">
+<input placeholder="Пароль" type="password" id="form_password" style="display: block;">
+<div class="form-button login_btn" style="display: block;">Войти</div>
+<div class="form-button clear_btn" style="display: block;">Сбросить local storage</div>
 </center>
 </div>
 `);
@@ -33,6 +34,10 @@ $(document).ready(function() {
   $(document).on('click', '.login_btn', function() {
                 login($( "#form_login" ).val(),$( "#form_password" ).val());
               console.log("Кнопка нажата");
+            });
+   
+     $(document).on('click', '.clear_btn', function() {
+   storage.clear();
             });
                  
                  
