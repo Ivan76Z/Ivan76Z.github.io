dfdcb674ec77498d7de6c8f3158e777c7ac8cf55
Ivan Zoyko <Ivan76Z@yandex.ru>
2021-05-12 01:30:37 +0300

    Update login.js

--- login.js
+++ login.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {
    $('body').prepend(`
- <div class="search_b" style="position: absolute; z-index:999; width: fit-content; top:0; left: 0;" onclick="localStorage.clear()">Сбросить local storage</div>
+ <div class="search_b storage-clear" onclick="localStorage.clear()">Сбросить local storage</div>
    `);
    function login_form(){
      $('body').prepend(`
-<div style="position: absolute; z-index:999; width:100vw; height: 100vh; top:0; right: 0; bottom: 0; left: 0; background-color: #fff;">
+<div class="login-div">
 <center>
 <h2 style="margin: 50px 0 0 0;">Вход</h2>
-<input placeholder="Логин" type="text" id="form_login" style="display: block; margin: 25px 0 0 0;">
-<input placeholder="Пароль" type="password" id="form_password" style="display: block; margin: 25px 0 0 0;">
-<div class="search_b login_btn" style="display: block; margin: 25px 0 0 0; width: fit-content;">Войти</div>
+<input placeholder="Логин" type="text" id="form_login" class="form-input search_i">
+<input placeholder="Пароль" type="password" id="form_password" class="form-input search_i">
+<div class="search_b login_btn">Войти</div>
 </center>
 </div>
 `);
@@ -19,15 +19,16 @@ $(document).ready(function() {
     $.getJSON('users.json', function(data) {
         //let data = JSON.stringify(obj);
         for (var i = 1; i <= data.length + 1; i++) {
-           if (a==data[i-1]["login"] && b==data[i-1]["password"]){
+           if (a==data[i-1]["login"]){
+              if (b==data[i-1]["password"]) {
                      localStorage.setItem('project_auth',true);
         location.reload();
-           } else {
-              alert("Неверный логин или пароль!");
+              } else {
+                 alert("Неверный логин или пароль");
+              }
            }
         }
-    });
-        
+    });    
    }
    
   $(document).on('click', '.login_btn', function() {
@@ -35,12 +36,7 @@ $(document).ready(function() {
             });
    
    function verification(){
-      console.log("Сетайтем в веривикации = " + localStorage.getItem('project_auth'));
      return (localStorage.getItem('project_auth'))? true : login_form();
    }
-  
   verification();
-    
-
-  
  });
