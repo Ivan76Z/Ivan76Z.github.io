d1b315cc8055079deafdb447b3ae2a32791fda1f
Ivan Zoyko <Ivan76Z@yandex.ru>
2021-05-12 01:37:35 +0300

    Update login.js

--- login.js
+++ login.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
-   $('body').prepend(`
+    $('body').prepend(`
  <div class="search_b storage-clear" onclick="localStorage.clear()">Сбросить local storage</div>
    `);
-   function login_form(){
-     $('body').prepend(`
+
+    function login_form() {
+        $('body').prepend(`
 <div class="login-div">
 <center>
 <h2 style="margin: 50px 0 0 0;">Вход</h2>
@@ -13,30 +14,29 @@ $(document).ready(function() {
 </center>
 </div>
 `);
-   }
-   
-     function login(a,b){
-    $.getJSON('users.json', function(data) {
-        //let data = JSON.stringify(obj);
-        for (var i = 1; i <= data.length + 1; i++) {
-           if (a==data[i-1]["login"]){
-              if (b==data[i-1]["password"]) {
-                     localStorage.setItem('project_auth',true);
-        location.reload();
-              } else {
-                 alert("Неверный логин или пароль");
-              }
-           }
-        }
-    });    
-   }
-   
-  $(document).on('click', '.login_btn', function() {
-                login($( "#form_login" ).val(),$( "#form_password" ).val());
-            });
-   
-   function verification(){
-     return (localStorage.getItem('project_auth'))? true : login_form();
-   }
-  verification();
- });
+    }
+
+    function login(a, b) {
+        $.getJSON('users.json', function(data) {
+            //let data = JSON.stringify(obj);
+            for (var i = 1; i <= data.length + 1; i++) {
+                if (a == data[i - 1]["login"] && b == data[i - 1]["password"]) {
+                    localStorage.setItem('project_auth', true);
+                    location.reload();
+                }
+            }
+            if (localStorage.getItem('project_auth') != true) {
+                alert(Неверный логин или пароль);
+            }
+        });
+    }
+
+    $(document).on('click', '.login_btn', function() {
+        login($("#form_login").val(), $("#form_password").val());
+    });
+
+    function verification() {
+        return (localStorage.getItem('project_auth')) ? true : login_form();
+    }
+    verification();
+});
